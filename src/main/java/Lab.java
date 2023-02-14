b4dab4b75bbf8a4c08d61399c93be9b4a756582b
ParkerChesney <parkerchesney@gmail.com>
2023-02-14 21:27:50 +0000

    Lab derp = new Lab();         return derp;

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -18,6 +18,7 @@ public class Lab {
      * @return any object.
      */
     public Object instantiateANewObject(){
-        return null;
+        Lab derp = new Lab();
+        return derp;
     }
 }
